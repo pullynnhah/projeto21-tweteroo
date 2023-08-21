2139b3f33c444f8dabed42dda572cba8cfab7301
Paula Rodrigues <pullynnhah@icloud.com>
2023-08-21 14:16:35 -0300

    feat: create user/tweet entities

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { User } from "./entities/user.entity";
+import { Tweet } from "./entities/tweet.entity";
 
 @Injectable()
 export class AppService {
+  private usersDB: User[];
+  private tweetsDB: Tweet[];
+
+  constructor() {
+    this.usersDB = [];
+    this.tweetsDB = [];
+  }
+
   getHello(): string {
     return "Hello World!";
   }
